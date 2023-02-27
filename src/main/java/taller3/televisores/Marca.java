6f40df15f4dfcbf031b70026f8c0901b3cfb6482
Milord01 <124930730+Milord01@users.noreply.github.com>
2023-02-27 18:16:40 -0500

    TV

---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,14 @@
+package taller3.televisores;
+
+public class Marca {
+	public String nombre;
+	public Marca(String str) {
+		nombre=str;
+	}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public String getNombre() {
+	return nombre;
+}
+}
---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,108 @@
+package taller3.televisores;
+
+public class TV {
+	
+	
+	
+	// Atributo_
+	public Marca[] marca;
+	public int canal=1;
+	public int precio=500;
+	public boolean estado;
+	public int volumen=1;
+	public Control[] control; 
+	private static int numTV; 
+	//constructor
+	public TV(Marca[] m, boolean e) {
+		numTV++;
+		marca=m;
+		estado=e;
+	}
+	
+	//metodos_set_y_get
+	public void setMarca(Marca[] marca) {
+		this.marca = marca;
+	}
+    public Marca[] getMarca() {
+	    return marca;
+    }
+    public void setControl(Control[] control) {
+		this.control = control;
+	}
+    public Control[] getControl() {
+		return control;
+	}
+    public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+    public int getPrecio() {
+		return precio;
+	}
+    public void setVolumen(int volumen) {
+		this.volumen = volumen;
+	}
+    public int getVolumen() {
+		return volumen;
+	}
+    public void setCanal(int canal) {
+		this.canal = canal;
+	}
+    public int getCanal() {
+		return canal;
+	}
+    
+    //Contador_numTV
+    public static int numTV() {
+    	return numTV;
+    	}
+    	
+    
+    //Encendido_o_Apagado
+    public boolean turnOn(){
+    	return true;
+    }
+    public boolean turnOff() {
+    	return false;
+    }
+    // getEstado
+    public boolean getEstado() {
+    	return estado;
+    }
+  
+    //canal
+    public int canalUp() {
+    	if(turnOn()==true) {
+    		if(canal>1&&canal<120) {
+    			canal=canal+1;
+    		}
+    	}
+    	return canal;
+    }
+  
+    public int canalDown() {
+    	if(turnOn()==true) {
+    		if(canal>1&&canal<120) {
+    			canal=canal-1;
+    		}
+    	}
+    	return canal;
+    }
+    
+    //volumen_
+    public int volumenUp() {
+    	if(turnOn()==true) {
+    		if(volumen>0&&volumen<7) {
+    			volumen=volumen+1;
+    		}
+    	}
+    	return volumen;
+    }
+    public int volumenDown() {
+    	if(turnOn()==true) {
+    		if(volumen>0&&volumen<7) {
+    			volumen=volumen-1;
+    		}
+    	}
+    	return volumen;
+    }
+}
